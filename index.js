24f499cb03b35f1b427492e5e6fb167d232a6bb5
Karan Gandhi <karangandhi.programming@gmail.com>
2020-03-11 22:09:20 +0530

    Compleatef the basic connection between clients

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ app.use(express.static("public"));
 const io = require("socket.io")(server);
 
 let clients = [];
+let sockets = [];
+let roomCounter = 0;
+let rooms = [];
 
 app.get("/clients", (req, res) => res.json({ clients }));
 
-io.on("connection", (socket) => clients.push(socket.id));
+io.on("connection", (socket) => {
+    let roomId;
+    clients.push(socket.id);
+    sockets.push(socket);
+
+    io.to(`${socket.id}`).emit("socketId", socket.id);
+
+    socket.on("connectionConfirmation", (data) => {
+        console.log(data);
+        if (data.status === true) {
+            roomId = roomCounter;
+            roomCounter++;
+            rooms.push({ ids: data.ids });
+        }
+        io.to(`${rooms[roomId].ids[0]}`).emit("connectionSucess", true);
+        io.to(`${rooms[roomId].ids[1]}`).emit("connectionSucess", true);
+    });
+
+    socket.on("requestConnection", (id) => io.to(`${id}`).emit("connectionRequest", socket.id));
+    socket.on("disconnect", () => clients.splice(clients.indexOf(socket.id), 1));
+});
--- public/src/main.js
+++ public/src/main.js
@@ -1,17 +1,35 @@
-const socket = io();
+const socket = io.connect(window.location.href);
+let id, root, clients;
 
 window.onload = () => load();
 
 async function load() {
-    let clients = await getClients();
+    clients = await getClients();
+    root = document.getElementById("root");
+    for (let i = 0; i < clients.clients.length; i++) {
+        let div = document.createElement("div");
+        div.innerHTML = clients.clients[i];
+        div.addEventListener("click", () => {
+            let id_ = clients.clients[i];
+            requestConnection(id_);
+        });
+        root.append(div);
+    }
 }
 
 async function getClients() {
-    let clients = [];
     let api = "/clients";
     let response = await fetch(api);
-    let data = await response.text();
-    console.log(data);
-
-    return clients;
+    let data = await response.json();
+    return data;
 }
+
+socket.on("socketId", (data) => (id = data));
+socket.on("connectionRequest", (data) => socket.emit("connectionConfirmation", { status: confirm(data + " is asking you to connect"), ids: [id, data] }));
+socket.on("connectionSucess", (data) => console.log(data));
+
+function requestConnection(id) {
+    socket.emit("requestConnection", id);
+}
+
+function requestToCall(numb) {}
